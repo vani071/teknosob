373af409d6aaf60984b294fc3031fa0256764bc8
root <root@teknosob.localhost>
2019-12-01 19:42:54 +0700

    initialize github

---
+++ storage/cms/cache/1e/8c/footer.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5de3b5195aad7118680708_dab979ceb198ad1feebce89d9b80de83Class extends Cms\Classes\PartialCode
+{
+
+}
---
+++ storage/cms/twig/1b/1b50bd97a8c1721a6b2b1c153172e407258d3d6fee183f1a933b65a09fdf3b4d.php
@@ -0,0 +1,52 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* /var/www/teknosob/themes/teknosob/partials/footer.htm */
+class __TwigTemplate_3ae52327652c261f0abd340e94625415bb592e26bfe6a09823b5ad23f1bb50a8 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+    }
+
+    public function getTemplateName()
+    {
+        return "/var/www/teknosob/themes/teknosob/partials/footer.htm";
+    }
+
+    public function getDebugInfo()
+    {
+        return array ();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "/var/www/teknosob/themes/teknosob/partials/footer.htm", "");
+    }
+}
